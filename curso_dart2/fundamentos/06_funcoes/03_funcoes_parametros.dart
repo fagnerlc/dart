920659705d1d23df62d2748dba9fa92c2ab14695
fagnerlc <fagnerlc@outlook.com>
2020-04-09 16:36:32 -0300

    atualizar

--- curso_dart2/fundamentos/06_funcoes/03_funcoes_parametros.dart
+++ curso_dart2/fundamentos/06_funcoes/03_funcoes_parametros.dart
@@ -1,3 +1,10 @@
+///
+/// Funcoes com parametros posicionados devem obedecer a ordem declarada!
+/// Funcoes com parametros nomeados devem ser declarados!
+/// Os parametros posicionados ou nomeados devem comecar pelo ultimo parametro da funcao!
+///
+
+
 funcaoParametros(){
   print('06.3.1) Funcoes com parametros posicionados e deffault.\n');
 
@@ -15,8 +22,22 @@ funcaoParametros(){
   }
 
   exibirDados2("Fagner");
-  exibirDados2("Fagner", peso: 95, altura: 1.89);
+  exibirDados2("Fagner", peso: 95, altura: 1.89); // pode declarar fora de ordem!
 
+  print('06.3.3) Funcoes como parametros para outras funcoes.\n');
+
+  falar(){
+    print('Essa é uma funcao passada como parametro');
+  }
+
+  void saudacao(String nome, {Function funcaoFalar}){
+    print('Olá, eu sou $nome!');
+    funcaoFalar();
+  }
+
+  saudacao('Fagner', funcaoFalar: falar);
+
+  saudacao('Fagner', funcaoFalar:() => print('Essa é uma funcao anonima passada como parametro nomeado!'));
 }
 
 void main() {
