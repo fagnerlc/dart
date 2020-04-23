432cf86a34962d96f45100403c7e3520ce656ca0
falc <fagnerlc@outlook.com>
2020-04-22 22:59:03 -0300

    aula de statics

---
+++ curso_dart2/fundamentos/10_modificadores/01_static/static.dart
@@ -0,0 +1,35 @@
+/*
+  * Static
+  A variavel estatica fica disponivel na prorpia classe em vez de suas instancias/objetos
+  Em metodos estaticos deve usar variaveis estaticas da classe
+  Variaveis estaticas pode, ser acessadas sem precisar instanciar classes/objetos 
+*/
+
+import '02_calculos.dart';
+
+void main(List<String> args) {
+  print('10.0) Modificadores Static \n');
+
+  double raio = 5;
+  var calc1 = new Calculos();
+  print(calc1.pi);
+  print(calc1.areaCirculo(raio));
+
+  print('');
+
+  var calc2 = new Calculos();
+  print(calc2.pi);
+  print(calc2.areaCirculo(raio));
+  //print(calc2.areaCirculoEstatico(raio));
+  print(Calculos.areaCirculoEstatico(raio));
+
+  print('\nInstancias consomem ${identical(calc1, calc2) ? 'MENOS' : 'MAIS'} memoria!\n');
+
+  print(Calculos.piEstatico);
+  print(Calculos.piEstatico * (raio * raio));
+  print('Retornos iguais: ${Calculos.areaCirculoEstatico(raio) == Calculos().areaCirculo(raio) ? 'Sim' : 'Nao'} Metodos diferentes!');
+
+
+  Calculos calc3 = Calculos();
+}
+
---
+++ curso_dart2/fundamentos/10_modificadores/02_final/01_final.dart
@@ -0,0 +1,5 @@
+/*
+  * Final
+    Inicializada apenas quando acessada em tempo de execução, deve ser declarada!
+    Em coleções final nem todo o conteudo sera final 
+ */
