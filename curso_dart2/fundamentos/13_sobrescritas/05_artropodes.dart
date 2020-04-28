e4f416c6e711c2068255f72559edc366502cf580
falc <fagnerlc@outlook.com>
2020-04-28 17:39:14 -0300

    aula desafio sobrescrita

---
+++ curso_dart2/fundamentos/13_sobrescritas/05_artropodes.dart
@@ -0,0 +1,25 @@
+import '02_animal.dart';
+
+abstract class Artropodes extends Animal{
+
+  String sexo;
+  String fazer;
+  //String insetos;
+  //String aracnideos;
+  //String crustaceos;
+
+  Artropodes.insetos(this.sexo, idade, {docil}) : super.invertebrados(idade, docil: docil){
+    this.fazer = 'Faz picada';
+  }
+  Artropodes.aracnideos(this.sexo, idade, {docil}) : super.invertebrados(idade, docil: docil){
+    this.fazer = 'Faz teia';
+  }
+  Artropodes.crustaceos(this.sexo, idade, {docil}) : super.invertebrados(idade, docil: docil){
+    this.fazer = 'Faz perolas';
+  }
+
+
+  void alimentar(){
+    print('Se alimenta como um ');
+  }
+}
---
+++ curso_dart2/fundamentos/13_sobrescritas/06_abelha.dart
@@ -0,0 +1,13 @@
+import '05_artropodes.dart';
+
+class Abelha extends Artropodes{
+
+  String nome;
+  String raca;
+  Abelha(this.nome, this.raca, String sexo, {String idade, bool docil = false} ) : super.insetos(sexo, idade, docil: docil);
+
+  void picar(){
+    print('Pica como uma abelha!');
+  }
+  
+}
