99bfacea5550d769c4e25bf674bdcf1c5f5b25d3
fagnerlc <fagnerlc@outlook.com>
2020-05-23 11:11:58 -0300

    aula testes 2

--- curso_dart2/fundamentos/19_testes/projeto/bin/main.dart
+++ curso_dart2/fundamentos/19_testes/projeto/bin/main.dart
@@ -1,3 +1,20 @@
+/**
+ *  * Assert
+ *    - Usa logica boleana para testar afirmações no modo debug!
+ *    - Try/Catch pode ser usado para tratar a exceção!
+ * 
+ *  * Teste
+ *    -  Certifique-se de ter o package "teste" em  "dev_dependences" no arquivo pubspec.yaml
+ *    - Atualize os packages no terminal usando "pub get"
+ *    - Execute os testes na pasta raiz usando "pub run test"
+ *    - Podemos testar variaveis, funcoes e efetuar testes em grupos
+ * 
+ *  * Sintaxe
+       teste("Descrição", (){
+         expect(valorReal, valorEsperado)
+       });
+ */
+
 import 'package:projeto/projeto.dart' as projeto;
 
 void main(List<String> arguments) {
