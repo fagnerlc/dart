a443839c576ea9e5a26e41e2ad41b7a9cc1cd3a6
fagnerlc <fagnerlc@outlook.com>
2020-05-23 10:58:52 -0300

    inicio de aula de testes

---
+++ curso_dart2/fundamentos/19_testes/projeto/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:projeto/projeto.dart' as projeto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto.calculate()}!');
+}
---
+++ curso_dart2/fundamentos/19_testes/projeto/test/projeto_test.dart
@@ -0,0 +1,8 @@
+import 'package:projeto/projeto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
