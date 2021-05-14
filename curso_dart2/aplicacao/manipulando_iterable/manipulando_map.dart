581444724b2afcbe17ff7eaaa7ba527d3646fead
Fagner Carvalho <fagnerlc@outlook.com>
2021-05-13 23:44:27 -0300

    entrando mais profundamente no map

--- curso_dart2/aplicacao/manipulando_iterable/manipulando_map.dart
+++ curso_dart2/aplicacao/manipulando_iterable/manipulando_map.dart
@@ -74,33 +74,36 @@ Map<String, dynamic> mapBolao = {
 
 main(List<String> args) {
   var map1 = mapBolao['bolao1'].firstWhere((element) => element['id'] == 1);
-  print('Primeiro Print: $map1');
+  print('Primeiro Print: $map1\n');
   var map2 = map1['campeonato'].firstWhere(
       (e) => e['titulo'] == 'Campeonato Brasileiro A')['classificacaoGamers'];
-  print('Segundo Print: $map2');
+  print('Segundo Print: $map2\n');
   var map3;
   map2.forEach((e) => map3 = e['nome']);
-  print('Terceiro Print: $map3');
+  print('Terceiro Print: $map3\n');
 
   var map4 = map1['campeonato']
       .firstWhere((e) => e['titulo'] == 'Campeonato Brasileiro A')['times'];
-  print('Quarto Print: $map4');
+  print('Quarto Print: $map4\n');
 
   var map5 = map4.firstWhere((e) => e['nome'] == 'Corinthians')['nome'];
-  print('Quinto Print: $map5');
+  print('Quinto Print: $map5\n');
 
   var map6 = map1['campeonato']
       .firstWhere((e) => e['titulo'] == 'Campeonato Brasileiro A')['fase'];
-  print('Sexto Print: $map6');
-
-  var map7 = mapBolao['bolao1']
-      .firstWhere((e) => e['id'] == 1)['campeonato']
-      .firstWhere((e) => e['titulo'] == 'Campeonato Brasileiro A')[
-          'classififasecacaoGamers']
-      .firstWhere(
-          (e) => e['grupo'].firstWhere((e) => e['titulo'] == 'Série A'));
-
-  //var map7 = map6.firstWhere((e) => e['titulo'] == 'Série A')['tabela'];
+  print('Sexto Print: $map6\n');
+  //var map7 = map6
+  //    .forEach((e) => e.firstWhere((e) => ['titulo'] == 'Série A')['tabela']);
   //.firstWhere((e) => e['grupo']);
-  print('Sétimo Print: $map7');
+  //print('Sétimo Print: $map7\n');
+
+  var nome1;
+  mapBolao['bolao1']
+      .firstWhere((e) => e['id'] == 1)['campeonato']
+      .firstWhere((e) => e['titulo'] == 'Campeonato Brasileiro A')['fase']
+      .forEach((e) => e['grupo']
+          .forEach((e) => e['tabela'].forEach((e) => nome1 = e['rodada'])));
+  //.firstWhere((e) => nome2 = e['rodada'])
+
+  print(' Nome 1 :$nome1\n');
 }
